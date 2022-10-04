5a8b041965bda86d9e81ce435f9db78e21a5bfcb
gusavato <augusto.abad@outlook.com>
2022-10-04 18:36:01 +0200

    04/10/2022

--- 0_Fizz_Buzz.py
+++ 0_Fizz_Buzz.py
@@ -19,4 +19,11 @@
 #
 
 for i in range(0,101):
-    if i\3 == 0:
+    if i%3 == 0 and i%5==0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
