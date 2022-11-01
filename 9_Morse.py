69e0356403c4c1e98f924dd0af7c666be635ab7f
gusavato <augusto.abad@outloock.com>
2022-11-01 08:56:18 +0100

    reto 9 creado

---
+++ 9_Morse.py
@@ -0,0 +1,12 @@
+# Reto #9
+# CÓDIGO MORSE
+# Fecha publicación enunciado: 02/03/22
+# Fecha publicación resolución: 07/03/22
+# Dificultad: MEDIA
+#
+# Enunciado: Crea un programa que sea capaz de transformar texto natural a código morse y viceversa.
+# - Debe detectar automáticamente de qué tipo se trata y realizar la conversión.
+# - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
+# - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
+
+####
