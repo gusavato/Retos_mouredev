4a58c6728362ddf90b1549f87a5e041aa7926a9b
gusavato <augusto.abad@outloock.com>
2022-11-08 19:07:02 +0100

    Creada tabla morse + inicio función es_morse

--- 9_Morse.py
+++ 9_Morse.py
@@ -18,6 +18,7 @@ def es_morse(txt):
         print("No se ha introducido una cadena de texto")
     elif
 
+
 # Importamos desde la wikipedia la tabla de código morse y la formateamos
 
 wiki_morse = pd.read_html('https://es.wikipedia.org/wiki/C%C3%B3digo_morse')
@@ -29,4 +30,3 @@ morse = dict()
 for i in range(0, wiki_morse[2].shape[1], 3):
     morse.update(dict(wiki_morse[2].loc[:, i:i + 1].values.tolist()))
 morse.pop(np.nan)
-
