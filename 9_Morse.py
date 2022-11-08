c9c629e15d34d609763ffc71421ba92988e12187
gusavato <augusto.abad@outloock.com>
2022-11-08 19:06:31 +0100

    Creada tabla morse + inicio función es_morse

--- 9_Morse.py
+++ 9_Morse.py
@@ -9,4 +9,24 @@
 # - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
 # - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
 
-####
+import pandas as pd
+import numpy as np
+
+
+def es_morse(txt):
+    if type(txt) != str:
+        print("No se ha introducido una cadena de texto")
+    elif
+
+# Importamos desde la wikipedia la tabla de código morse y la formateamos
+
+wiki_morse = pd.read_html('https://es.wikipedia.org/wiki/C%C3%B3digo_morse')
+wiki_morse[2] = wiki_morse[2].set_axis(list(range(0, wiki_morse[2].shape[1])), axis=1, copy=False)
+
+# Creamos un diccionario con todos los morse
+
+morse = dict()
+for i in range(0, wiki_morse[2].shape[1], 3):
+    morse.update(dict(wiki_morse[2].loc[:, i:i + 1].values.tolist()))
+morse.pop(np.nan)
+
