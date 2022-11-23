b00fbe45a6a23bec1eefeb5650aca2e91a2f817f
gusavato <augusto.abad@outloock.com>
2022-11-23 15:41:18 +0100

    Creación reto 10

---
+++ 10_Expresiones_equilibradas.py
@@ -0,0 +1,12 @@
+# Reto #10
+# EXPRESIONES EQUILIBRADAS
+# Fecha publicación enunciado: 07/03/22
+# Fecha publicación resolución: 14/03/22
+# Dificultad: MEDIA
+
+# Enunciado: Crea un programa que comprueba si los paréntesis,
+# llaves y corchetes de una expresión están equilibrados.
+# - Equilibrado significa que estos delimitadores se abren y cieran en orden y de forma correcta.
+# - Paréntesis, llaves y corchetes son igual de prioritarios. No hay uno más importante que otro.
+# - Expresión balanceada: { [ a #( c + d ) ] - 5 }
+# - Expresión no balanceada: { a #( c + d ) ] - 5 }
