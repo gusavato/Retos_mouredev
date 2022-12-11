46f3ce8d454ec6c55fb473eece677751c3864de0
gusavato <augusto.abad@outlook.com>
2022-12-11 14:57:02 +0100

    Creado

--- 15_Cuantos_dias.py
+++ 15_Cuantos_dias.py
@@ -1,12 +1,16 @@
- # Reto #15
- # ¿CUÁNTOS DÍAS?
- # Fecha publicación enunciado: 11/04/22
- # Fecha publicación resolución: 18/04/22
- # Dificultad: DIFÍCIL
- #
- # Enunciado: Crea una función que calcule y retorne cuántos días
- # hay entre dos cadenas de texto que representen fechas.
- # - Una cadena de texto que representa una fecha tiene el formato "dd/MM/yyyy".
- # - La función recibirá dos String y retornará un Int.
- # - La diferencia en días será absoluta (no importa el orden de las fechas).
- # - Si una de las dos cadenas de texto no representa una fecha correcta se lanzará una excepción.
+# Reto #15
+# ¿CUÁNTOS DÍAS?
+# Fecha publicación enunciado: 11/04/22
+# Fecha publicación resolución: 18/04/22
+# Dificultad: DIFÍCIL
+#
+# Enunciado: Crea una función que calcule y retorne cuántos días
+# hay entre dos cadenas de texto que representen fechas.
+# - Una cadena de texto que representa una fecha tiene el formato "dd/MM/yyyy".
+# - La función recibirá dos String y retornará un Int.
+# - La diferencia en días será absoluta (no importa el orden de las fechas).
+# - Si una de las dos cadenas de texto no representa una fecha correcta se lanzará una excepción.
+
+from datetime import datetime
+
+print(datetime(year=2022, month=12, day=12,))
